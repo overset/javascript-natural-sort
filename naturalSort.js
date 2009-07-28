1fecbb393e41d3efa37eaf41431ce2be4e2189dc
Jim Palmer <jimpalmer@gmail.com>
2009-07-28 19:53:41 +0000

    initial merge into code.google svn - starting with version 0.3 with added fixes and 10x speedup by Mike Grier

---
+++ naturalSort.js
@@ -0,0 +1,30 @@
+/*
+ * Natural Sort algorithm for Javascript
+ *  Version 0.3
+ * Author: Jim Palmer (based on chunking idea from Dave Koelle)
+ *  optimizations and safari fix by Mike Grier (mgrier.com)
+ * Released under MIT license.
+ */
+function naturalSort(a, b){
+	// setup temp-scope variables for comparison evauluation
+	var re = /(-?[0-9\.]+)/g,
+		x = a.toString().toLowerCase() || '',
+		y = b.toString().toLowerCase() || '',
+		nC = String.fromCharCode(0),
+		xN = x.replace( re, nC + '$1' + nC ).split(nC),
+		yN = y.replace( re, nC + '$1' + nC ).split(nC),
+		xD = (new Date(x)).getTime(),
+		yD = xD ? (new Date(y)).getTime() : null;
+	// natural sorting of dates
+	if ( yD )
+		if ( xD < yD ) return -1;
+		else if ( xD > yD )	return 1;
+	// natural sorting through split numeric strings and default strings
+	for( var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++ ) {
+		oFxNcL = parseFloat(xN[cLoc]) || xN[cLoc];
+		oFyNcL = parseFloat(yN[cLoc]) || yN[cLoc];
+		if (oFxNcL < oFyNcL) return -1;
+		else if (oFxNcL > oFyNcL) return 1;
+	}
+	return 0;
+}
