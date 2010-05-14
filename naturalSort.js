6d6352a832394bc43deb2b62ac031692ce69753b
Jim Palmer <jimpalmer@gmail.com>
2010-05-14 08:30:01 +0000

    first round of unit tests via qunit + added lookahead to chunking regexp as per Kyle Adam's suggestion 'Ideally there would be some sort of intelligence that checks to see if a numeric chunk contains multiple '.'. If it does, break it up into smaller numeric chunks using the '.' as a separator. Otherwise treat it as a float.' which allows better detection of floats + trim whitespace preventing chunking getting thrown off + ensure that we do not compare against undefined values which easily happens when one value has more chunks than the other value being compared against - ensuring it's either a float or a string to not throw off the default comparison operators + Date() detection might be too loose - now preventing '1.2.4' from being a valid date, or any comparison value with a '.' in it to allow floats and versioning numbers to properly sort

--- naturalSort.js
+++ naturalSort.js
@@ -1,28 +1,27 @@
 /*
- * Natural Sort algorithm for Javascript
- *  Version 0.3
+ * Natural Sort algorithm for Javascript - Version 0.4 - Released under MIT license
  * Author: Jim Palmer (based on chunking idea from Dave Koelle)
- *  optimizations and safari fix by Mike Grier (mgrier.com)
- * Released under MIT license.
+ * Contributors: Mike Grier (mgrier.com), Clint Priest, Kyle Adams
  */
 function naturalSort(a, b){
 	// setup temp-scope variables for comparison evauluation
-	var re = /(-?[0-9\.]+)/g,
-		x = a.toString().toLowerCase() || '',
-		y = b.toString().toLowerCase() || '',
+	var re = /([0-9]+(?:\.[0-9]*)?)/g,
+		sre = /(^[ ]*|[ ]*$)/g,
+		x = a.toString().toLowerCase().replace(sre, '') || '',
+		y = b.toString().toLowerCase().replace(sre, '') || '',
 		nC = String.fromCharCode(0),
-		xN = x.replace( re, nC + '$1' + nC ).split(nC),
-		yN = y.replace( re, nC + '$1' + nC ).split(nC),
+		xN = x.replace(re, nC + '$1' + nC).split(nC),
+		yN = y.replace(re, nC + '$1' + nC).split(nC),
 		xD = (new Date(x)).getTime(),
 		yD = xD ? (new Date(y)).getTime() : null;
-	// natural sorting of dates
-	if ( yD )
+	// natural sorting of dates - prevent '1.2.3' valid date
+	if ( y.indexOf('.') < 0 && yD )
 		if ( xD < yD ) return -1;
 		else if ( xD > yD )	return 1;
 	// natural sorting through split numeric strings and default strings
-	for( var cLoc = 0, numS = Math.max(xN.length, yN.length); cLoc < numS; cLoc++ ) {
-		oFxNcL = parseFloat(xN[cLoc]) || xN[cLoc];
-		oFyNcL = parseFloat(yN[cLoc]) || yN[cLoc];
+	for( var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++ ) {
+		oFxNcL = parseFloat(xN[cLoc]) || xN[cLoc] || '';
+		oFyNcL = parseFloat(yN[cLoc]) || yN[cLoc] || '';
 		if (oFxNcL < oFyNcL) return -1;
 		else if (oFxNcL > oFyNcL) return 1;
 	}
