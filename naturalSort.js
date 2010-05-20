d8332afed8de6e9d6c7c6a314c0bb1308e671417
Jim Palmer <jimpalmer@gmail.com>
2010-05-20 04:55:18 +0000

    compare hex along with date outside of tokenize + minor cleanup

--- naturalSort.js
+++ naturalSort.js
@@ -5,25 +5,26 @@
  */
 function naturalSort(a, b){
 	// setup temp-scope variables for comparison evauluation
-	var re = /(^[0-9]+\.?[0-9]*[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/g,
+	var re = /(^[0-9]+\.?[0-9]*[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
 		sre = /(^[ ]*|[ ]*$)/g,
-		hre = /^0x[0-9a-f]+$/,
+		hre = /^0x[0-9a-f]+$/i,
 		ore = /^0/,
-		nC = '\0',
-		x = a.toString().toLowerCase().replace(sre, '') || '',
-		y = b.toString().toLowerCase().replace(sre, '') || '',
-		// first look for hex values otherwise chunk/tokenize
-		xN = (parseInt(x.match(hre)) || x.replace(re, nC + '$1' + nC)).toString().split(nC),
-		yN = (parseInt(y.match(hre)) || y.replace(re, nC + '$1' + nC)).toString().split(nC),
-		xD = (new Date(x)).getTime(),
-		yD = xD ? (new Date(y)).getTime() : null;
-	// natural sorting of dates - prevent '1.2.3' valid date
+		// convert all to strings and trim()
+		x = a.toString().replace(sre, '') || '',
+		y = b.toString().replace(sre, '') || '',
+		// chunk/tokenize
+		xN = x.replace(re, '\0$1\0').split('\0'),
+		yN = y.replace(re, '\0$1\0').split('\0'),
+		// hex or date detection
+		xD = parseInt(x.match(hre)) || (new Date(x)).getTime(),
+		yD = parseInt(y.match(hre)) || xD && (new Date(y)).getTime() || null;
+	// natural sorting of hex or dates - prevent '1.2.3' valid date
 	if ( y.indexOf('.') < 0 && yD )
 		if ( xD < yD ) return -1;
 		else if ( xD > yD )	return 1;
 	// natural sorting through split numeric strings and default strings
-	for( var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++ ) {
-		// find floats not starting with '0', string or 0 if not defined
+	for(var cLoc=0, numS=Math.max(xN.length, yN.length); cLoc < numS; cLoc++) {
+		// find floats not starting with '0', string or 0 if not defined (Clint Priest)
 		oFxNcL = !(xN[cLoc] || '').match(ore) && parseFloat(xN[cLoc]) || xN[cLoc] || 0;
 		oFyNcL = !(yN[cLoc] || '').match(ore) && parseFloat(yN[cLoc]) || yN[cLoc] || 0;
 		// handle numeric vs string comparison - number < string - (Kyle Adams)
