3630ebd260c3fbfc3909e2c58ca0f5e91ccc3bc0
Jim Palmer <jimpalmer@gmail.com>
2011-01-26 01:38:02 +0000

    lose date handling to wrangle chrome's very forgiving Date.parse() + better detection for sorting negative numbers

--- naturalSort.js
+++ naturalSort.js
@@ -1,13 +1,14 @@
 /*
- * Natural Sort algorithm for Javascript - Version 0.4 - Released under MIT license
+ * Natural Sort algorithm for Javascript - Version 0.5 - Released under MIT license
  * Author: Jim Palmer (based on chunking idea from Dave Koelle)
- * Contributors: Mike Grier (mgrier.com), Clint Priest, Kyle Adams
+ * Contributors: Mike Grier (mgrier.com), Clint Priest, Kyle Adams, guillermo
  */
 function naturalSort(a, b){
 	// setup temp-scope variables for comparison evauluation
-	var re = /(^[0-9]+\.?[0-9]*[df]?e?[0-9]?$|^0x[0-9a-f]+$|[0-9]+)/gi,
+	var re = /(^-?[0-9]+(\.?[0-9]*)[df]?e?[0-9]?$|^0x[0-9a-f]+$|-?[0-9]+)/gi,
 		sre = /(^[ ]*|[ ]*$)/g,
 		hre = /^0x[0-9a-f]+$/i,
+		dre = /(^[0-9\-\.\/]{5,}$)|[0-9]+:[0-9]+|( [0-9]{4})/i,
 		ore = /^0/,
 		// convert all to strings and trim()
 		x = a.toString().replace(sre, '') || '',
@@ -15,11 +16,11 @@ function naturalSort(a, b){
 		// chunk/tokenize
 		xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
 		yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
-		// hex or date detection
-		xD = parseInt(x.match(hre)) || (new Date(x)).getTime(),
+		// numeric, hex or date detection
+		xD = parseInt(x.match(hre)) || (xN.length != 1 && x.match(dre) && (new Date(x)).getTime()),
 		yD = parseInt(y.match(hre)) || xD && (new Date(y)).getTime() || null;
 	// natural sorting of hex or dates - prevent '1.2.3' valid date
-	if ( y.indexOf('.') < 0 && yD )
+	if (yD)
 		if ( xD < yD ) return -1;
 		else if ( xD > yD )	return 1;
 	// natural sorting through split numeric strings and default strings
