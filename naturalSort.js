3eabc73718da57eaf00dafd2aef4fa9fdceac9bc
Jim Palmer <jim@atomicmgmt.com>
2016-01-29 03:04:20 -0800

    version 0.8.1 + added better unicode locale support fixing https://github.com/overset/javascript-natural-sort/issues/9 + added regex lookahead for better chunking with mixed number/strings + fix for https://github.com/overset/javascript-natural-sort/issues/13#issuecomment-175064008

--- naturalSort.js
+++ naturalSort.js
@@ -1,9 +1,9 @@
 /*
- * Natural Sort algorithm for Javascript - Version 0.8 - Released under MIT license
+ * Natural Sort algorithm for Javascript - Version 0.8.1 - Released under MIT license
  * Author: Jim Palmer (based on chunking idea from Dave Koelle)
  */
 function naturalSort (a, b) {
-    var re = /(^([+\-]?(?:\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?)?$|^0x[\da-fA-F]+$|\d+)/g,
+    var re = /(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g,
         sre = /^\s+|\s+$/g,   // trim pre-post whitespace
         snre = /\s+/g,        // normalize all whitespace to single ' ' character
         dre = /(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,
@@ -13,8 +13,8 @@ function naturalSort (a, b) {
             return (naturalSort.insensitive && ('' + s).toLowerCase() || '' + s).replace(sre, '');
         },
         // convert all to strings strip whitespace
-        x = i(a) || '',
-        y = i(b) || '',
+        x = i(a),
+        y = i(b),
         // chunk/tokenize
         xN = x.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
         yN = y.replace(re, '\0$1\0').replace(/\0$/,'').replace(/^\0/,'').split('\0'),
@@ -28,22 +28,23 @@ function naturalSort (a, b) {
         oFxNcL, oFyNcL;
     // first try and sort Hex codes or Dates
     if (yD) {
-        if ( xD < yD ) { return -1; }
-        else if ( xD > yD ) { return 1; }
+        if (xD < yD) { return -1; }
+        else if (xD > yD) { return 1; }
     }
     // natural sorting through split numeric strings and default strings
-    for(var cLoc=0, xNl = xN.length, yNl = yN.length, numS=Math.max(xNl, yNl); cLoc < numS; cLoc++) {
+    for(var cLoc = 0, xNl = xN.length, yNl = yN.length, numS = Math.max(xNl, yNl); cLoc < numS; cLoc++) {
         oFxNcL = normChunk(xN[cLoc] || '', xNl);
         oFyNcL = normChunk(yN[cLoc] || '', yNl);
         // handle numeric vs string comparison - number < string - (Kyle Adams)
-        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) { return (isNaN(oFxNcL)) ? 1 : -1; }
-        // rely on string comparison if different types - i.e. '02' < 2 != '02' < '2'
-        else if (typeof oFxNcL !== typeof oFyNcL) {
-            oFxNcL += '';
-            oFyNcL += '';
+        if (isNaN(oFxNcL) !== isNaN(oFyNcL)) {
+            return isNaN(oFxNcL) ? 1 : -1;
+        }
+        // if unicode use locale comparison
+        if (/[^\x00-\x80]/.test(oFxNcL + oFyNcL) && oFxNcL.localeCompare) {
+            var comp = oFxNcL.localeCompare(oFyNcL);
+            return comp / Math.abs(comp);
         }
         if (oFxNcL < oFyNcL) { return -1; }
-        if (oFxNcL > oFyNcL) { return 1; }
+        else if (oFxNcL > oFyNcL) { return 1; }
     }
-    return 0;
 }
